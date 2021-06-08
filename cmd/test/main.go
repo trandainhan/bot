dca705b0739057ff230754cb148964103a4aa3f3
Nhan Tran <trdainhan@gmail.com>
2021-06-08 15:28:10 +0700

    chore: test script

--- cmd/test/main.go
+++ cmd/test/main.go
@@ -6,15 +6,15 @@ import (
 	"log"
 	"os"
 
-	// "gitlab.com/fiahub/bot/internal/binance"
+	"gitlab.com/fiahub/bot/internal/binance"
 	"gitlab.com/fiahub/bot/internal/rediswrapper"
 )
 
 func main() {
 	// Test login
-	email := "trdainhan@gmail.com"
-	password := "Nahn@291992"
-	token := fiahub.Login(email, password)
+	// email := "trdainhan@gmail.com"
+	// password := "Nahn@291992"
+	// token := fiahub.Login(email, password)
 	// log.Println(token)
 	// rate, _ := fiahub.GetUSDVNDRate()
 	// log.Println(rate)
@@ -27,29 +27,35 @@ func main() {
 	redisURL := os.Getenv("REDIS_URL")
 	redisClient := rediswrapper.NewRedisClient(ctx, redisURL)
 	redisClient.Set("nhantran", "hello")
-	// log.Println(redisClient.Get("nhantran"))
+	log.Println(redisClient.Get("nhantran"))
 
 	// bid_order: {coin_amount: 3, price_per_unit_cents: 23731, type: "BidOrder"
 	// coin: "USDT", currency: "VNT"}
-	order := fiahub.Order{
-		CoinAmount:        3,
-		PricePerUnitCents: 7848,
-		Type:              "AskOrder",
-		Coin:              "DOGE",
-		Currency:          "VNT",
-	}
-	resp, _ := fiahub.CreateAskOrder(token, order)
-	log.Println(resp)
+	// order := fiahub.Order{
+	// 	CoinAmount:        3,
+	// 	PricePerUnitCents: 7848,
+	// 	Type:              "AskOrder",
+	// 	Coin:              "DOGE",
+	// 	Currency:          "VNT",
+	// }
+	// resp, _ := fiahub.CreateAskOrder(token, order)
+	// log.Println(resp)
 	// resp, _, _ := fiahub.CancelOrder(token, 103184704)
 	// log.Println(resp)
 
-	// Test binance api
-	// offset := binance.GetOffsetTimeUnix()
-	// redisClient.Set("local_binance_time_difference", offset)
-
-	// bn := binance.Binance{
-	// 	RedisClient: redisClient,
+	// detail, _, err := fiahub.GetAskOrderDetails("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoyNzU1NywiY29kZSI6IncyajhXIiwiZXhwIjoxNjI1NzE5MzY4fQ.Mt6PNJDEhHkxTPIiodwiz_W_0bXfr-BLAAyvDgJXbb0", 103411475)
+	// if err != nil {
+	// 	panic(err)
 	// }
+	// log.Println(detail)
+
+	// Test binance api
+	offset := binance.GetOffsetTimeUnix()
+	redisClient.Set("local_binance_time_difference", offset)
+
+	bn := binance.Binance{
+		RedisClient: redisClient,
+	}
 	//
 	// usdtFund := bn.CheckFund("USDT")
 	// log.Println(usdtFund)
@@ -63,4 +69,14 @@ func main() {
 	// bidPriceByQuantity, askPriceByQuantity := binance.GetPriceByQuantity("DOGEUSDT", 8.0)
 	// log.Printf("bidPriceByQuantity: %v", bidPriceByQuantity)
 	// log.Printf("askPriceByQuantity: %v", askPriceByQuantity)
+	// resp, err := bn.BuyLimit("DOGEUSDT", 0.3, 100)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// log.Println(resp.OrderID)
+	// log.Println(resp)
+
+	resp, _ := bn.GetOrder("DOGEUSDT", 1239188099, "SLYJI2yBT99GaIo4qc35iM")
+	log.Println(resp)
+
 }
