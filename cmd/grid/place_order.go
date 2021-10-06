c2c557b1f0f6ac87ec44bff016bf1422ee02ff71
Nhan Tran <trdainhan@gmail.com>
2021-10-06 11:13:13 +0700

    improve log

--- cmd/grid/place_order.go
+++ cmd/grid/place_order.go
@@ -26,8 +26,8 @@ func placeOrder(botID string, quantity, exchancePrice float64, side string) (*ex
 }
 
 func notifyFailedOrder(botID string, sellQuantity, totalUSDT float64, orderType string, err error) {
-	text := fmt.Sprintf("Err %s: Can not make order on Exchange %s %s", os.Getenv("TELEGRAM_HANDLER"), coin, botID)
-	text = fmt.Sprintf("%s =========================: %s LIMIT: Quantity: %v TotalUSDT %v Error: %s", text, orderType, sellQuantity, totalUSDT, err)
+	text := fmt.Sprintf("%s Err Can not make order on Exchange %s %s\n", os.Getenv("TELEGRAM_HANDLER"), coin, botID)
+	text = fmt.Sprintf("%s Quantity: %v TotalUSDT %v Error: %s", text, sellQuantity, totalUSDT, err)
 	log.Println(text)
 	go teleClient.SendMessage(text, chatErrorID)
 	time.Sleep(5 * time.Second)
