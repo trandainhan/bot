8dc36ba54837f3bd3f56affe712d8118948caa04
Nhan Tran <trdainhan@gmail.com>
2021-10-06 11:10:22 +0700

    update atom bin file

--- bin/atom.sh
+++ bin/atom.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-grid -coin=ATOM -decimalsToRound=3 -quantityToGetPrice=5 -orderQuantity=3 -jumpPricePercentage=1 -buySellDiffSize=12 -maximumOrderUsdt=120
+grid -coin=ATOM -decimalsToRound=2 -orderQuantityToRound=1 -quantityToGetPrice=5 -orderQuantity=4 -jumpPricePercentage=1 -buySellDiffSize=16 -maximumOrderUsdt=160
