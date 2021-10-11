64ab3deffef2418b75b3cbc363b6b786b84e2247
Nhan Tran <trdainhan@gmail.com>
2021-10-11 15:27:12 +0700

    update bnb2 bin file

--- bin/bnb2.sh
+++ bin/bnb2.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-grid2 -coin=BNB -decimalsToRound=1 -quantityToGetPrice=1 -orderQuantity=0.5 -jumpPricePercentage=0.5 -maximumOrderUsdt=500
+grid2 -coin=BNB -numWorker=5 -decimalsToRound=1 -quantityToGetPrice=1 -orderQuantity=0.25 -jumpPricePercentage=0.1 -maximumOrderUsdt=400
