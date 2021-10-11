48382b3d3da77499d6476a20403990887a12d469
Nhan Tran <trdainhan@gmail.com>
2021-10-11 22:27:11 +0700

    update rune2 bin file

--- bin/rune2.sh
+++ bin/rune2.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-grid2 -coin=RUNE -decimalsToRound=3 -numWorker=5 -quantityToGetPrice=20 -orderQuantity=10 -jumpPricePercentage=0.2 -maximumOrderUsdt=120
+grid2 -coin=RUNE -decimalsToRound=3 -numWorker=5 -quantityToGetPrice=20 -orderQuantity=10 -jumpPricePercentage=0.1 -maximumOrderUsdt=120
