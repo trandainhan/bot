68c19961b6703a7c44372f95f7c8f3f74eed1d32
Nhan Tran <trdainhan@gmail.com>
2021-10-14 22:40:09 +0700

    update bin file

--- bin/bnb.sh
+++ bin/bnb.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-grid -coin=BNB -decimalsToRound=1 -orderQuantityToRound=1 -quantityToGetPrice=1 -orderQuantity=0.25 -jumpPricePercentage=0.1 -buySellDiffSize=1.25  -maximumOrderUsdt=150
+grid -coin=BNB -decimalsToRound=1 -orderQuantityToRound=1 -quantityToGetPrice=1 -orderQuantity=0.2 -jumpPricePercentage=0.1 -buySellDiffSize=1.4 -maximumOrderUsdt=150
--- bin/rune.sh
+++ bin/rune.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-grid -coin=RUNE -decimalsToRound=3 -orderQuantityToRound=1 -quantityToGetPrice=20 -orderQuantity=10 -jumpPricePercentage=0.2 -maximumOrderUsdt=120
+grid -coin=RUNE -decimalsToRound=3 -orderQuantityToRound=1 -quantityToGetPrice=20 -orderQuantity=10 -jumpPricePercentage=0.2 -buySellDiffSize=70  -maximumOrderUsdt=120
